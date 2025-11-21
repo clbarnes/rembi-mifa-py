8365aea14538560ef4fce49cac3a59df9e53677f
Chris Barnes <chris.barnes@gerbi-gmb.de>
2025-11-21 16:47:14 +0000

    add metadata and __version__

--- src/rembi_mifa/__init__.py
+++ src/rembi_mifa/__init__.py
@@ -5,5 +5,9 @@ as described by the EBI BioImage Archive: https://www.ebi.ac.uk/bioimage-archive
 
 from . import rembi
 from . import mifa
+from importlib.metadata import version as _importlib_metadata_version
+
+__version__ = _importlib_metadata_version("rembi-mifa")
+
 
 __all__ = ["rembi", "mifa"]
